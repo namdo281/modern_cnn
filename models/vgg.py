ccc659b41efcb98588d8ca9c319182e5422b53a2
namdo <donam.2801@gmail.com>
2022-03-01 16:49:56 +0700

    vgg done and trainable

--- models/vgg.py
+++ models/vgg.py
@@ -32,7 +32,8 @@ class VGGNet(nn.Module):
         self.linear1 = nn.Linear(6272, 256)
         self.dropout1 = nn.Dropout(0.5)
         self.linear2 = nn.Linear(256, 256)
-        self.dropout2 = nn.Dropout(0.5) 
+        self.dropout2 = nn.Dropout(0.5)
+        self.linear3 = nn.Linear(256, 10) 
         #print(self.vm)
     def forward(self, x):
         #print(x.shape)
@@ -44,6 +45,7 @@ class VGGNet(nn.Module):
         x = self.dropout1(x)
         x = self.linear2(x)
         x = self.dropout2(x)
+        x = self.linear3(x)
         x = F.log_softmax(x, dim=-1)
         return x
 
